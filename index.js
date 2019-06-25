5b864f33922ace4beb2cdd2ef22b29a32fe168ca
Joanne Badua <joannebadua@gmail.com>
2019-06-24 23:06:35 -0700

    pseudocode: choose word from answers array by creating an empty array and using the forloop in order to create a new letter object and push it into an empty array

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ var data = require("./word.js");
 var lettersArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var answersArray = ["nike","adidas","puma"];
-var randomAnswerIndex = math.floor(math.random() *answersArray.length);
+// choose word from answers array
+// first, create an empty Array, use a forloop for everytime, 
+//create a new letter object and push it into the empy array
+var randomAnswerIndex = math.floor(math.random() * answersArray.length);
+var randomAnswer;
+var guessesLeft;
+
+function startGame(){
+
+    answersArray = [];
+    for (var i = 0; i < answersArray[randomAnswerIndex].length; i++)
+    {
+
+    }
+}
+
+function userInput(){
+    //inquirer to have the user input letter 
+    inquirer.prompt
+}
+
