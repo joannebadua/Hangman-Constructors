02d0faec66c983ab5897b5729bb3e9484a2497e3
Joanne Badua <joannebadua@gmail.com>
2019-06-24 18:11:10 -0700

    switch case for letter nonLetter

--- letter.js
+++ letter.js
@@ -4,9 +4,17 @@ var Letter = function(letter) {
   // this.students will hold all of our student objects
   this.letter = letter;
   this.guessedLetter = false;
-  this.print = function() {
-    // *switch for entering letter, space or _
+  this.print = function () {
+     switch(letterPlaceholder) {
+       case "letter":
+       return this.letter;
+       break;
+       case "non-letter":
+       return "_";
+       break;
+     }
   },
+  letterPlaceholder();
 
 this.letterCheck = function(check) {
     if((check === this.letter {
