291e87217793912fe801efdc33f999b96e21a1a5
Joanne Badua <joannebadua@gmail.com>
2019-06-24 21:34:16 -0700

    add lettersArray and answersArray and random mathfloor for answerindex

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var inquirer = require("inquirer");
 var data = require("./word.js");
+
+var lettersArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var answersArray = ["nike","adidas","puma"];
+var randomAnswerIndex = math.floor(math.random() *answersArray.length);
